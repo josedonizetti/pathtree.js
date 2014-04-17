7c498419eee3342201dbc11ce39de54ad01b16cb
Jose Donizetti <jdbjunior@gmail.com>
2014-04-16 22:15:18 -0400

    supporting multiple node levels

--- test/pathtree_test.js
+++ test/pathtree_test.js
@@ -4,9 +4,19 @@ var assert = require('assert'),
 describe('Pathtree', function() {
   it('basic route', function() {
     var pathTree = new PathTree()
+
     pathTree.add("/", 0)
+    pathTree.add("/path", 1)
+    pathTree.add("/path/to", 2)
+
 
     var leaf = pathTree.find("/")
     assert.equal(leaf.value, 0)
+
+    leaf = pathTree.find("/path")
+    assert.equal(leaf.value, 1)
+
+    leaf = pathTree.find("/path/to")
+    assert.equal(leaf.value, 2)
   })
 })
